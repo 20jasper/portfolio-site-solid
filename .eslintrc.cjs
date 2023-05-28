a43c06616d485f9a35451f5c102e76ce32a1828e
Jacob Asper <jacobasper191@gmail.com>
2023-05-28 19:22:28 -0400

    add eslint packages and config (#7)

---
+++ .eslintrc.cjs
@@ -0,0 +1,36 @@
+/**
+ * @type {import("eslint").Linter.Config}
+ */
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:astro/recommended",
+    "plugin:astro/jsx-a11y-strict",
+    "plugin:@typescript-eslint/strict",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "2021",
+    sourceType: "module",
+    jsx: true,
+    project: "./tsconfig.json",
+  },
+  plugins: ["@typescript-eslint", "only-warn"],
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+  ignorePatterns: ["dist/**/*", ".eslintrc.*"],
+};
