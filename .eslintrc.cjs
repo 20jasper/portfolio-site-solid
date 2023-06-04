f018786b2eaeab18c9ea2d3049c9d4ec685b24d0
Jacob Asper <jacobasper191@gmail.com>
2023-06-04 13:22:51 -0400

    only use solid linter with solid and astro linter with astro

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,48 +2,61 @@
  * @type {import("eslint").Linter.Config}
  */
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    // eslint recommended
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    //typescript 
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:@typescript-eslint/strict",
-    // solid/astro
-    "plugin:solid/typescript",
-    "plugin:astro/recommended",
-    // a11y
-    "plugin:astro/jsx-a11y-strict",
-    "plugin:jsx-a11y/strict",
-  ],
-  rules: {
-    "@typescript-eslint/explicit-function-return-type": "warn",
-    "@typescript-eslint/consistent-type-definitions": ["warn", "type"]
-  },
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaVersion: "2021",
-    sourceType: "module",
-    jsx: true,
-    project: "./tsconfig.json",
-  },
-  plugins: ["@typescript-eslint", "solid", "astro", "jsx-a11y", "only-warn"],
-  overrides: [
-    {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
-      parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
-      },
-      rules: {},
-    },
-  ],
-  ignorePatterns: ["dist/**/*", ".eslintrc.*"],
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		// eslint recommended
+		'eslint:recommended',
+		'plugin:@typescript-eslint/eslint-recommended',
+		//typescript
+		'plugin:@typescript-eslint/recommended',
+		'plugin:@typescript-eslint/recommended-requiring-type-checking',
+		'plugin:@typescript-eslint/strict',
+	],
+	rules: {
+		'@typescript-eslint/explicit-function-return-type': 'warn',
+		'@typescript-eslint/consistent-type-definitions': ['warn', 'type'],
+	},
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: '2021',
+		sourceType: 'module',
+		jsx: true,
+		project: './tsconfig.json',
+	},
+	plugins: ['@typescript-eslint', 'jsx-a11y', 'only-warn'],
+	overrides: [
+		{
+			files: ['*.astro'],
+			parser: 'astro-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+				extraFileExtensions: ['.astro'],
+			},
+			plugins: ['astro'],
+			extends: ['plugin:astro/recommended', 'plugin:astro/jsx-a11y-strict'],
+			rules: {},
+		},
+		{
+			files: ['*.tsx'],
+			parserOptions: {
+				parser: '@typescript-eslint/parser',
+			},
+			plugins: ['solid'],
+			overrides: ['plugin:solid/typescript', 'plugin:jsx-a11y/strict'],
+			rules: {
+				'solid/self-closing-comp': [
+					'warn',
+					{
+						component: 'all', // "all" | "none"
+						html: 'void', // "all" | "void" | "none"
+					},
+				],
+			},
+		},
+	],
+	ignorePatterns: ['dist/**/*', '.eslintrc.*'],
 };
