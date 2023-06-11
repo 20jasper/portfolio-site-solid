4ebaefc753c1879626329736e7afffddd51d5801
Jacob Asper <jacobasper191@gmail.com>
2023-06-11 01:12:54 -0400

    reorder keys in projects file

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -2,14 +2,14 @@ import type { Props as Project } from '../../components/Project.astro';
 
 const projects: Project[] = [
 	{
+		projectName: 'five line rhyme',
 		demoUrl: 'https://five-line-rhyme.up.railway.app/',
 		gitHubUrl: 'https://github.com/20jasper/five-line-rhyme',
-		projectName: 'five line rhyme',
 		description: 'A collection of poems only five lines long',
 		image: {
+			src: 'https://user-images.githubusercontent.com/78604367/192409376-b33d90a4-9132-434e-bb98-b6867c74443b.png',
 			altText:
 				'The home page of five line rhyme. There are a few user–submitted poems to read',
-			src: 'https://user-images.githubusercontent.com/78604367/192409376-b33d90a4-9132-434e-bb98-b6867c74443b.png',
 		},
 		techUsed: [
 			'JavaScript',
@@ -22,14 +22,14 @@ const projects: Project[] = [
 		],
 	},
 	{
+		projectName: 'budget tracker',
 		demoUrl: 'https://budget-tracker.up.railway.app/',
 		gitHubUrl: 'https://github.com/20jasper/budget-tracker',
-		projectName: 'budget tracker',
 		description: 'Track your monthly expenses',
 		image: {
+			src: 'https://user-images.githubusercontent.com/78604367/188524492-5c3a6633-335d-4511-ac35-59dc073f138c.png',
 			altText:
 				'A budget planner with a few test expenses. There is a form to add a new expense',
-			src: 'https://user-images.githubusercontent.com/78604367/188524492-5c3a6633-335d-4511-ac35-59dc073f138c.png',
 		},
 		techUsed: [
 			'JavaScript',
