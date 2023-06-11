85c74c05c7bc431a499439f29a04977ec3566aa1
Jacob Asper <jacobasper191@gmail.com>
2023-06-10 22:44:52 -0400

    5 add prettier (#33)
    * add recommended extensions
    
    * set prettier as default formatter
    
    * add prettier config
    
    * only use solid linter with solid and astro linter with astro
    
    * put html attributes on same line
    
    * add formatting scripts
    
    * check formatting in ci
    
    * format

---
+++ .prettierrc.cjs
@@ -0,0 +1,19 @@
+/**
+ * @type {import("prettier").Config}
+ */
+module.exports = {
+	plugins: [require.resolve('prettier-plugin-astro')],
+	overrides: [
+		{
+			files: '*.astro',
+			options: {
+				parser: 'astro',
+			},
+		},
+	],
+	semi: true,
+	tabWidth: 2,
+	singleQuote: true,
+	useTabs: true,
+	singleAttributePerLine: true,
+};
