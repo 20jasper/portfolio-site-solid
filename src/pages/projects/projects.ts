abd4097b6577428003da93a927103c03f0f36dfb
Jacob Asper <jacobasper191@gmail.com>
2023-06-14 18:20:36 -0400

    allow videos in projects

--- src/pages/projects/projects.ts
+++ src/pages/projects/projects.ts
@@ -1,11 +1,13 @@
 import type { Props as Project } from '../../components/Project.astro';
+import fullStackInterviewQuestions from '../../../public/images/fullStackInterviewQuestions.mp4';
 
 const projects: Project[] = [
 	{
 		projectName: 'five line rhyme',
 		demoUrl: 'https://five-line-rhyme.up.railway.app/',
 		description: 'A collection of poems only five lines long',
-		image: {
+		media: {
+			mediaType: 'image',
 			src: 'https://user-images.githubusercontent.com/78604367/192409376-b33d90a4-9132-434e-bb98-b6867c74443b.png',
 			altText:
 				'The home page of five line rhyme. There are a few user–submitted poems to read',
@@ -25,7 +27,8 @@ const projects: Project[] = [
 		projectName: 'budget tracker',
 		demoUrl: 'https://budget-tracker.up.railway.app/',
 		description: 'Track your monthly expenses',
-		image: {
+		media: {
+			mediaType: 'image',
 			src: 'https://user-images.githubusercontent.com/78604367/188524492-5c3a6633-335d-4511-ac35-59dc073f138c.png',
 			altText:
 				'A budget planner with a few test expenses. There is a form to add a new expense',
@@ -46,7 +49,8 @@ const projects: Project[] = [
 		projectName: 'PAIHS Portfolios',
 		demoUrl: 'https://paihsportfolios.netlify.app/',
 		description: 'Track your monthly expenses',
-		image: {
+		media: {
+			mediaType: 'image',
 			src: 'https://user-images.githubusercontent.com/78604367/190449712-5d834ec4-354c-4a35-953b-47ff17f5c5b1.png',
 			altText:
 				'A large purple crest with hands handing the globe below a large purple heading that says PAIHS portfolios',
@@ -58,8 +62,10 @@ const projects: Project[] = [
 		projectName: 'Full Stack Interview Question Generator',
 		demoUrl: 'https://full-stack-interview-prep.up.railway.app/',
 		description: 'Study for a full stack web development interview',
-		image: {
-			src: 'images/fullStackInterviewQuestions.mp4',
+		media: {
+			mediaType: 'video',
+			videoType: 'video/mp4',
+			src: fullStackInterviewQuestions,
 			altText:
 				'An animation of a button being clicked and a question showing up',
 		},
