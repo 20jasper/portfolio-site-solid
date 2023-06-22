640a9eff7171df06aee7fd01968c0a762e006997
Jacob Asper <jacobasper191@gmail.com>
2023-06-21 23:01:19 -0400

    remove unused javascript

--- public/scripts/main.cjs
+++ public/scripts/main.cjs
@@ -5,20 +5,8 @@
 */
 
 (function ($) {
-	var $window = $(window),
-		$body = $('body'),
-		$menu = $('#menu'),
-		$sidebar = $('#sidebar'),
-		$main = $('#main');
-
-	// Breakpoints.
-	breakpoints({
-		xlarge: ['1281px', '1680px'],
-		large: ['981px', '1280px'],
-		medium: ['737px', '980px'],
-		small: ['481px', '736px'],
-		xsmall: [null, '480px'],
-	});
+	var $body = $('body');
+	var $menu = $('#menu');
 
 	// Menu.
 	$menu.appendTo($body).panel({
@@ -30,17 +18,5 @@
 		side: 'right',
 		target: $body,
 		visibleClass: 'is-menu-visible',
-	});
-
-	// Intro.
-	var $intro = $('#intro');
-
-	// Move to main on <=large, back to sidebar on >large.
-	breakpoints.on('<=large', function () {
-		$intro.prependTo($main);
-	});
-
-	breakpoints.on('>large', function () {
-		$intro.prependTo($sidebar);
 	});
 })(jQuery);
